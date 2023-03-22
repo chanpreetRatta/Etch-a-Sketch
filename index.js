6e09f842f75e1c87c0d7b2ddda494a4935d2bde4
Chanpreet <chanpreetlopon@gmail.com>
2023-03-22 15:45:00 -0700

    implement the grid

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+let container = document.getElementsByClassName("container")[0];
+let div = document.createElement("div");
+div.classList = "innerDIV";
 
+let scroll = document.querySelector(".controllers")[0].value;
+console.log(scroll);
+
+for (let divElement = 0; divElement <= 960; divElement++) {
+  container.appendChild(div.cloneNode());
+}
+
+container.childNodes.forEach((element) =>
+  element.addEventListener("mouseover", (event) => {
+    event.target.style = "background-color: black";
+  })
+);
