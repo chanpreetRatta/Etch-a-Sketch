5ad36f535090c5d9a52082130d460564c4e73d5a
Chanpreet <chanpreetlopon@gmail.com>
2023-03-23 19:59:11 -0700

    impliment grid size and the pixel size

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-let container = document.getElementsByClassName("container")[0];
+let container = document.getElementsByClassName("container");
+let controlPanel = document.querySelector(".controllers");
 let div = document.createElement("div");
-div.classList = "innerDIV";
+let divSize = localStorage.getItem("divSize");
+let containerSize = localStorage.getItem("containerSize");
 
-let scroll = document.querySelector(".controllers")[0].value;
-console.log(scroll);
+controlPanel.addEventListener("submit", (event) => {
+  event.preventDefault();
+  localStorage.setItem(
+    "containerSize",
+    controlPanel.elements["containerSize"].value
+  );
+  localStorage.setItem(
+    "divSize",
+    localStorage.getItem("containerSize") /
+      controlPanel.elements["pixels"].value
+  );
 
-for (let divElement = 0; divElement <= 960; divElement++) {
-  container.appendChild(div.cloneNode());
+  location.reload();
+});
+
+container[0].style["max-width"] = `${containerSize}px`;
+container[0].style["max-height"] = `${containerSize}px`;
+div.style["width"] = `${divSize}px`;
+div.style["height"] = `${divSize}px`;
+
+let calculateDiv = containerSize / divSize;
+console.log(divSize);
+
+for (let repeat = 0; repeat < calculateDiv * calculateDiv; repeat++) {
+  container[0].appendChild(div.cloneNode());
 }
 
-container.childNodes.forEach((element) =>
-  element.addEventListener("mouseover", (event) => {
-    event.target.style = "background-color: black";
-  })
-);
+container[0].addEventListener("mouseover", (event) => {
+  event.target.style["background-color"] = "black";
+});
